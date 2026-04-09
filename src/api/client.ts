3d993f93e554f0dc23e5e62be96fa181c1f0bdc9
Jerry <jerrylalrinmawia@gmail.com>
2026-04-09 16:48:56 +0530

    Client api

---
+++ src/api/client.ts
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import { readStoredSession } from '../utils/session'
+
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+})
+
+apiClient.interceptors.request.use((config) => {
+  const session = readStoredSession()
+
+  if (session?.token) {
+    config.headers?.set('Authorization', `Bearer ${session.token}`)
+  }
+
+  return config
+})
+
+export default apiClient
