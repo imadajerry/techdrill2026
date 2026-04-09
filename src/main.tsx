4d9b6d5521cc9c90e171aee4d8c47d0b22ed72ba
Jerry <jerrylalrinmawia@gmail.com>
2026-04-09 20:17:55 +0530

    Simplified due to code refactor

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
-import { Home } from 'lucide-react';
-import About from './pages/About.tsx';
-import Root from './layouts/Root.tsx';
-import AuthLayout from './layouts/AuthLayout.tsx';
-import Login from './pages/Login.tsx';
-import Register from './pages/Register.tsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Root,
-    children: [
-      { index: true, Component: Home },
-      { path: "about", Component: About },
-      {
-        path: "auth",
-        Component: AuthLayout,
-        children: [
-          { path: "login", Component: Login },
-          { path: "register", Component: Register },
-        ],
-      },
-    ],
-  },
-]);
-
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
