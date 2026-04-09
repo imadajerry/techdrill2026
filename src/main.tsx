551ca8567ee4d3ff1d30170fd724e1159bfe5a6b
Jerry <jerrylalrinmawia@gmail.com>
2026-04-09 16:45:02 +0530

    Used react-router for linking

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import { Home } from 'lucide-react';
+import About from './pages/About.tsx';
+import Root from './layouts/Root.tsx';
+import AuthLayout from './layouts/AuthLayout.tsx';
+import Login from './pages/Login.tsx';
+import Register from './pages/Register.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      { index: true, Component: Home },
+      { path: "about", Component: About },
+      {
+        path: "auth",
+        Component: AuthLayout,
+        children: [
+          { path: "login", Component: Login },
+          { path: "register", Component: Register },
+        ],
+      },
+    ],
+  },
+]);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
