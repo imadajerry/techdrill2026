505e7dab88bbd604a6420da9f3208d86eff69e44
Jerry <jerrylalrinmawia@gmail.com>
2026-04-09 18:32:07 +0530

    Customer layout

---
+++ src/components/layout/CustomerLayout.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom'
+import StorefrontFooter from './StorefrontFooter'
+import StorefrontHeader from './StorefrontHeader'
+import styles from './CustomerLayout.module.css'
+
+export default function CustomerLayout() {
+  return (
+    <div className={styles.page}>
+      <StorefrontHeader />
+      <main className={styles.content}>
+        <Outlet />
+      </main>
+      <StorefrontFooter />
+    </div>
+  )
+}
