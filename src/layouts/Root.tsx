3ddb3513541ec9c5879e720ae9fe28979686c298
Jerry <jerrylalrinmawia@gmail.com>
2026-04-09 18:35:05 +0530

    root layout

---
+++ src/layouts/Root.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom'
+import StorefrontFooter from '../components/layout/StorefrontFooter'
+import StorefrontHeader from '../components/layout/StorefrontHeader'
+import styles from '../components/layout/CustomerLayout.module.css'
+
+export default function Root() {
+  return (
+    <div className={styles.page}>
+      <StorefrontHeader />
+      <main className={styles.content}>
+        <Outlet />
+      </main>
+      <StorefrontFooter />
+    </div>
+  )
+}
