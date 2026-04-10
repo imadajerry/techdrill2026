5a6f5b1acdcbd96742bb7d344f00f7955e391ea3
Jerry <jerrylalrinmawia@gmail.com>
2026-04-10 09:26:21 +0530

    chatbot widget component

--- frontend/src/layouts/Root.tsx
+++ frontend/src/layouts/Root.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import StorefrontFooter from '../components/layout/StorefrontFooter'
 import StorefrontHeader from '../components/layout/StorefrontHeader'
+import ChatbotWidget from '../components/ui/ChatbotWidget'
 import styles from '../components/layout/CustomerLayout.module.css'
 
 export default function Root() {
@@ -11,6 +12,7 @@ export default function Root() {
         <Outlet />
       </main>
       <StorefrontFooter />
+      <ChatbotWidget />
     </div>
   )
 }
